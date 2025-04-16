e6f35360891f11d63cb6c6cade9386b53aa7731e
seoul-dev <jad3choi@gmail.com>
2025-04-16 22:14:02 +0900

    Depoly BaseNft on Holesky testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const { vars } = require("hardhat/config");
+
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+
+const HOLESKY_PRIVATE_KEY = vars.get("HOLESKY_PRIVATE_KEY");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    holesky: {
+      url: `https://eth-holesky.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [HOLESKY_PRIVATE_KEY],
+    },
+  },
 };
