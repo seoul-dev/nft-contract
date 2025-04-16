28a8eef426f3d38c80e069b6e811110f978f0c0e
seoul-dev <jad3choi@gmail.com>
2025-04-17 00:24:26 +0900

    Tidy files

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,15 +4,25 @@ const { vars } = require("hardhat/config");
 
 const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 
-const HOLESKY_PRIVATE_KEY = vars.get("HOLESKY_PRIVATE_KEY");
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+
+// npx hardhat vars set ETHERSCAN_API_KEY
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    holesky: {
-      url: `https://eth-holesky.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [HOLESKY_PRIVATE_KEY],
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
     },
   },
 };
+
+// npx hardhat ignition deploy ignition/modules/BaseNft.js --network sepolia --verify
